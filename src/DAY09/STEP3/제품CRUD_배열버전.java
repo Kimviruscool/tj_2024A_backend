f9f581f049a4a23d87b95202f529d2f6c66a1b7b
Kimviruscool <sinsa9122@naver.com>
2024-06-19 09:05:13 +0900

    240619

--- src/DAY09/STEP3/제품CRUD_배열버전.java
+++ src/DAY09/STEP3/제품CRUD_배열버전.java
@@ -2,15 +2,15 @@ package day09.step3;
 
 import java.util.Scanner;
 
-public class 제품CRUD_변수버전 {
+public class 제품CRUD_배열버전 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        String prodCode1=""; String prodName1=""; int prodPrice1=0;
-        String prodCode2=""; String prodName2=""; int prodPrice2=0;
-        String prodCode3=""; String prodName3=""; int prodPrice3=0;
-
+        String[] prodCode = new String[]{"","",""};
+        String[] prodName = new String[]{"","",""};
+        int[] prodPrice = new int[3];
+        
         // 제품 메모리 설계
 
         while(true){ // 무한반복
@@ -23,61 +23,61 @@ public class 제품CRUD_변수버전 {
                 System.out.print("제품가격 입력 : "); int inputPrice = scanner.nextInt();
                 if (inputPrice <= 0)
                     System.out.println("[경고] 제품가격이 잘못되었습니다.");
-                else if (!inputCode.equals("") && prodName1.equals("") && prodPrice1 == 0){
-                    prodCode1 = inputCode; prodName1 = inputName; prodPrice1 = inputPrice;
+                else if (!inputCode.equals("") && prodName[0].equals("") && prodPrice[0] == 0){
+                    prodCode[0] = inputCode; prodName[0] = inputName; prodPrice[0] = inputPrice;
                 }
-                else if (!inputCode.equals("") && prodName2.equals("") && prodPrice2 == 0){
-                    prodCode2 = inputCode; prodName2 = inputName; prodPrice2 = inputPrice;
+                else if (!inputCode.equals("") && prodName[1].equals("") && prodPrice[1] == 0){
+                    prodCode[1] = inputCode; prodName[1] = inputName; prodPrice[1] = inputPrice;
                 }
-                else if (!inputCode.equals("") && prodName3.equals("") && prodPrice3 == 0){
-                    prodCode3 = inputCode; prodName3 = inputName; prodPrice3 = inputPrice;
+                else if (!inputCode.equals("") && prodName[2].equals("") && prodPrice[2] == 0){
+                    prodCode[2] = inputCode; prodName[2] = inputName; prodPrice[2] = inputPrice;
                 }
                 else
                     System.out.println("[경고] 제품 등록 자리가 부족합니다");
             }
             else if (ch == 2){
                 System.out.println("=========== 제품목록 ===========");
-                if (!prodCode1.equals("") && !prodName1.equals("") && prodPrice1 != 0){
-                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode1, prodName1, prodPrice1);
+                if (!prodCode[0].equals("") && !prodName[0].equals("") && prodPrice[0] != 0){
+                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode[0], prodName[0], prodPrice[0]);
                 }
-                if (!prodCode2.equals("") && !prodName2.equals("") && prodPrice2 != 0){
-                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode2, prodName2, prodPrice2);
+                if (!prodCode[1].equals("") && !prodName[1].equals("") && prodPrice[1] != 0){
+                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode[1], prodName[1], prodPrice[1]);
                 }
-                if (!prodCode3.equals("") && !prodName3.equals("") && prodPrice3 != 0){
-                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode3, prodName3, prodPrice3);
+                if (!prodCode[2].equals("") && !prodName[2].equals("") && prodPrice[2] != 0){
+                    System.out.printf("\t %s \t %s \t\t %d\n", prodCode[2], prodName[2], prodPrice[2]);
                 }
             }
             else if (ch == 3){
                 System.out.print("수정할 제품코드 : "); String updateCode = scanner.next();
-                if (updateCode.equals(prodCode1) && !prodName1.equals("") && prodPrice1 != 0){
+                if (updateCode.equals(prodCode[0]) && !prodName[0].equals("") && prodPrice[0] != 0){
                     System.out.print("새로운 제품명 : "); String newName = scanner.next();
-                    if (!newName.equals("")) {prodName1 = newName;}
+                    if (!newName.equals("")) {prodName[0] = newName;}
                     System.out.print("새로운 제품 가격 : "); int newPrice = scanner.nextInt();
-                    if (newPrice >= 0){prodPrice1 = newPrice;}
+                    if (newPrice >= 0){prodPrice[0] = newPrice;}
                 }
-                else if (updateCode.equals(prodCode2) && !prodName2.equals("") && prodPrice2 != 0){
+                else if (updateCode.equals(prodCode[1]) && !prodName[1].equals("") && prodPrice[1] != 0){
                     System.out.print("새로운 제품명 : "); String newName = scanner.next();
-                    if (!newName.equals("")) {prodName2 = newName;}
+                    if (!newName.equals("")) {prodName[1] = newName;}
                     System.out.print("새로운 제품 가격 : "); int newPrice = scanner.nextInt();
-                    if (newPrice >= 0){prodPrice2 = newPrice;}
+                    if (newPrice >= 0){prodPrice[1] = newPrice;}
                 }
-                else if (updateCode.equals(prodCode3) && !prodName3.equals("") && prodPrice3 != 0){
+                else if (updateCode.equals(prodCode[2]) && !prodName[2].equals("") && prodPrice[2] != 0){
                     System.out.print("새로운 제품명 : "); String newName = scanner.next();
-                    if (!newName.equals("")) {prodName3 = newName;}
+                    if (!newName.equals("")) {prodName[2] = newName;}
                     System.out.print("새로운 제품 가격 : "); int newPrice = scanner.nextInt();
-                    if (newPrice >= 0){prodPrice3 = newPrice;}
+                    if (newPrice >= 0){prodPrice[2] = newPrice;}
                 }
                 else
                     System.out.println("[경고] 존재하지 않는 코드입니다.");
             }
             else if (ch == 4){
                 System.out.print("삭제할 제품 코드 : "); String delCode = scanner.next();
-                if (delCode.equals(prodCode1) && !prodName1.equals("") && prodPrice1 != 0){
-                    prodName1 = ""; prodPrice1 = 0;}
-                else if (delCode.equals(prodCode2) && !prodName1.equals("") && prodPrice1 != 0){
-                    prodName2 = ""; prodPrice2 = 0;}
-                else if (delCode.equals(prodCode3) && !prodName1.equals("") && prodPrice1 != 0){
-                    prodName3 = ""; prodPrice3 = 0;}
+                if (delCode.equals(prodCode[0]) && !prodName[0].equals("") && prodPrice[0] != 0){
+                    prodName[0] = ""; prodPrice[0] = 0;}
+                else if (delCode.equals(prodCode[1]) && !prodName[0].equals("") && prodPrice[0] != 0){
+                    prodName[1] = ""; prodPrice[1] = 0;}
+                else if (delCode.equals(prodCode[2]) && !prodName[0].equals("") && prodPrice[0] != 0){
+                    prodName[2] = ""; prodPrice[2] = 0;}
                 else
                     System.out.println("[경고] 존재하지 않는 코드입니다.");
             }
