7d612cc4de1184cabcc998365771926ab3177b5a
Kimviruscool <sinsa9122@naver.com>
2024-06-25 09:26:15 +0900

    24.06.25

--- src/DAY13/Step5/Step5.java
+++ src/DAY13/Step5/Step5.java
@@ -64,3 +64,24 @@ public class Step5 {
         myCar.run();
     }
 }
+
+
+/*
+
+해석
+1. 차 클래스를 생성 멤버변수로 타이어 , 메소드로 run(멤버변수tire를.roll()(다른 클래스의 메소드))을 추가.
+2. 타이어 클래스를 생성 메소드로 roll() , print를 추가.
+3. 타이어 클래스를 상속하는 한국타이어를 생성
+4. 타이어 클래스를 상속하는 금호타이어를 생성
+/////////////////////////////////////////////////
+5. mycar 객채를 생성
+6. yourcar 객채를 생성 후 yourcar객체 안의 tire객체를 생성 yourcar.run시 "기본타이어가 회전합니다." 출력
+7. mycar 객체에 tire객체를 생성 mycar.run시 "기본타이어가 회전합니다." 출력
+8. mycar 객체에 tire 상속 받는 한국타이어 객체를 생성 mycar.run시 "한국타이어가 회전합니다" 출력 //상위클래스보다 본인클래스를 우선시함
+9. mycar 객체에 tire 상속 받는 금호타이어 객체를 생성 mycar.run시 "금호타이어가 회전합니다."출력
+    yourcar객체는 변함 없이 tire만 포함되어있어 "기본타이어가 회전합니다." 출력
+10. System.out.println(myCar.tire instanceof KumhoTire); 시 mycar객체의 tire에는 override로 금호타이어 , 타이어어 타입이 포함되어있기때문에 true 출력
+11. if(mycar.tire 객체에 tire 가 포함되어있기때문에){mycar.tire 객체에 한국타이어 객체 생성}
+12. mycar.run 출력시 "한국타이어가 회전합니다." 출력.
+
+*/
